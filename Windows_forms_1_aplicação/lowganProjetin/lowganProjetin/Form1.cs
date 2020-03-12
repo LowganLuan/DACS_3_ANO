36f524b8fd6f81def1eaa6e2f081e2ac7810f704
Lowgan Luan Flores <40185705+LowganLuan@users.noreply.github.com>
2020-03-12 19:29:44 -0300

    Add files via upload

---
+++ Windows_forms_1_aplicação/lowganProjetin/lowganProjetin/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lowganProjetin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'univilleDataSet.clientes' table. You can move, or remove it, as needed.
+            this.clientesTableAdapter.Fill(this.univilleDataSet.clientes);
+
+        }
+
+        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.clientesTableAdapter.Update(this.univilleDataSet.clientes);
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            this.clientesTableAdapter.Fill(this.univilleDataSet.clientes);
+        }
+    }
+}
